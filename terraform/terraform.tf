784ad48f72522e041c4f88714131d394a87c4e5e
siddharthrawats4 <rawatsids4@gmail.com>
2025-06-29 14:23:05 +0530

    staging workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions021"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
